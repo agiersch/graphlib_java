e662ebcac472eba3abfd0d743d3382c860645928
Arnaud Giersch <arnaud.giersch@univ-fcomte.fr>
2014-10-13 14:49:13 +0200

    Fix name of window.

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ public class Hello {
     public static void main(String[] args) {
         // Création d'une fenêtre avec, en paramètres, son titre, sa
         // largeur et sa hauteur
-        DrawingWindow w = new DrawingWindow("Exemple 1", 640, 480);
+        DrawingWindow w = new DrawingWindow("Hello", 640, 480);
 
         // Affichage d'un court message au milieu de la fenêtre
         w.drawText(w.width / 2, w.height / 2, "Hello world!");
